9d5ddc07c2b99974c61eccdb1fc60ebebf8678d6
Lee Chun On <jeffreyleeon@gmail.com>
2017-09-07 22:22:58 -0700

    Implement bresenham line algo

---
+++ main.py
@@ -0,0 +1,48 @@
+from matplotlib import pyplot as plt
+
+f = plt.figure(1, figsize = (8,8))
+ax = f.gca()
+ax.set_xlim([0, 100])
+ax.set_ylim([0, 100])
+f.show()
+
+# Assuming x2 > x1 and y2 > y1
+x1 = 0.0
+y1 = 0.0
+x2 = 100.0
+y2 = 33.0
+
+dx = x2 - x1
+dy = y2 - y1
+slope = abs(dy / dx)
+
+error = 0.0
+y = y1
+x = x1
+
+while x < x2:
+    ax.plot(x, y, 'ko')
+    f.canvas.draw()
+    x = x + 1
+    error = error + slope
+    if error >= 0.5:
+        y = y + 1
+        error -= 1.0
+    raw_input('pause : press any key ...')
+f.close()
+
+'''
+ function line(x0, y0, x1, y1)
+     real deltax := x1 - x0
+     real deltay := y1 - y0
+     real deltaerr := abs(deltay / deltax)    // Assume deltax != 0 (line is not vertical),
+           // note that this division needs to be done in a way that preserves the fractional part
+     real error := 0.0 // No error at start
+     int y := y0
+     for x from x0 to x1 
+         plot(x,y)
+         error := error + deltaerr
+         if error >= 0.5 then
+             y := y + 1
+             error := error - 1.0
+'''
